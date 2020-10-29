71e89493f13dff9aec42b4e0428c9665af64988d
Ayobeen <abdlazeezmumeen@gmail.com>
2020-10-29 10:29:51 +0100

    initial commmit

--- loan_unit_test.py
+++ loan_unit_test.py
@@ -5,10 +5,10 @@ class TestTotalDays(unittest.TestCase):
 
     def test_sum(self):
         self.assertEqual(get_days_of_power(R1=1000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=21000), 10, "Should be 10")
-        self.assertEqual(get_days_of_power(R1=3000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=700000), 141, "Should be 10")
-        self.assertEqual(get_days_of_power(R1=500, D1=3, R2=500, D2=10, R3=500, D3=7, K=21000), 17, "Should be 10")
-        self.assertEqual(get_days_of_power(R1=1300, D1=0, R2=500, D2=0, R3=1500, D3=7, K=10000), 5, "Should be 10")
-        self.assertEqual(get_days_of_power(R1=10000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=11000), 1, "Should be 10")
+        self.assertEqual(get_days_of_power(R1=3000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=700000), 141, "Should be 141")
+        self.assertEqual(get_days_of_power(R1=500, D1=3, R2=500, D2=10, R3=500, D3=7, K=21000), 17, "Should be 17")
+        self.assertEqual(get_days_of_power(R1=1300, D1=0, R2=500, D2=0, R3=1500, D3=7, K=10000), 5, "Should be 5")
+        self.assertEqual(get_days_of_power(R1=10000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=11000), 1, "Should be 1")
 
 
 if __name__ == '__main__':
