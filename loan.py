d6bb7cfa40a46ca4bd18151f003afd853516aad7
Ayobeen <abdlazeezmumeen@gmail.com>
2020-10-29 09:33:11 +0100

    initial commmit

--- loan.py
+++ loan.py
@@ -41,4 +41,4 @@ def get_days_of_power(R1, D1, R2, D2, R3, D3, K):
 
 
 if __name__ == "__main__":
-    print(get_days_of_power(R1=1000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=3000))
+    print(get_days_of_power(R1=1000, D1=3, R2=500, D2=10, R3=1500, D3=7, K=21000))
